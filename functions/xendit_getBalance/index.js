ab63952c7f67db979cee061ca2922a745bcfba07
Antoni Angga <antoniangga14@gmail.com>
2017-09-06 17:30:13 +0700

    kadang2 data nya null jadi belum bisa

--- functions/xendit_getBalance/index.js
+++ functions/xendit_getBalance/index.js
@@ -1,15 +1,32 @@
 const axios = require("axios");
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
 
 exports.handle = function(e, ctx, cb) {
-    axios.get('https://api.xendit.co/balance',{ // API check Balance
-    auth: {
-        username: 'xnd_development_P4yEfOV1g7Ktl8I4fLAbTjOTZNalp9d5kXe0+Rxj+WPQ+banDAN1gw=='
-      }
-    })
-        .then(function (response) {
-        cb(null,response.data);
-    })
-        .catch(function (error) {
-        cb(error,null);
+    const params = {
+        QueueUrl: 'https://sqs.ap-southeast-1.amazonaws.com/455680218869/test_queue_1',
+        MaxNumberOfMessages: 1
+    };
+    sqs.receiveMessage(params, function(err, data) {
+        if (err) {
+            console.error(err, err.stack);
+            cb(err);
+        } else {
+            //SQS Recieve Message nya.
+            cb(null, data.Messages);
+            // ini untuk Axios nya
+            // axios.get('https://api.xendit.co/balance',{ // API check Balance
+            // auth: {
+            //     username: secret_key.MessageAttributes.secret_key
+            //   }
+            // })
+            //     .then(function (response) {
+            //     cb(null,response.data);
+            // })
+            //     .catch(function (error) {
+            //     cb(null,error);
+            // });
+        }
     });
+
 }
