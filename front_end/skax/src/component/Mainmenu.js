913d517b96ff741c3b0b67aef2e6eb77c52b3e85
Antoni Angga <antoniangga14@gmail.com>
2017-10-03 10:00:37 +0700

    last commit

---
+++ front_end/skax/src/component/Mainmenu.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+class Mainmenu extends Component {
+    state = { activeItem: 'home' }
+    
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state;
+        return (
+            <div>
+                <Menu pointing secondary>
+                    <Link to ="/mainmenu"> <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/> </Link>
+                    <Link to ="/workflow"> <Menu.Item name='workflow' active={activeItem === 'workflow'} onClick={this.handleItemClick}/> </Link>
+                    <Link to ="/history"> <Menu.Item name='history' active={activeItem === 'history'} onClick={this.handleItemClick}/> </Link>
+                    <Menu.Menu position='right'>
+                    <Link to="/"> <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick}/> </Link>
+                    </Menu.Menu>
+                </Menu>
+                <h1> ini tempat tarok Dashboard nanti </h1>
+            </div>
+        );
+    }
+}
+
+
+export default Mainmenu
